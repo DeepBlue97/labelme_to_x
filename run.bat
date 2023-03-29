4f8869def5cbeb84dd2bf6c4cd34b070b7a0cf6b
DeepBlue97 <945048933@qq.com>
2023-03-29 23:47:02 +0800

    labelme to voc ok

---
+++ run.bat
@@ -0,0 +1,3 @@
+python main.py D:\workspace\deep_learning\data\gtest out --labels labels.txt
+
+python json2xml.py --raw_label_dir D:\workspace\deep_learning\data\gtest --pic_dir D:\workspace\deep_learning\data\gtest --save_dir out2
